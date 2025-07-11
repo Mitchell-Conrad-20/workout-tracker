5b90c4b059de5f9960842491488211deb6d1f33c
Mitchell Conrad <Mitchell.conrad@gmail.com>
2025-07-10 21:12:03 -0400

    button and input components

---
+++ app/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  dark?: boolean;
+};
+
+const Button: React.FC<ButtonProps> = ({ children, dark = false, ...props }) => (
+  dark ? (
+    <button
+      {...props}
+      className='cursor-pointer rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors ease-in-out flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto'
+    >
+      {children}
+    </button>
+  ) : (
+    <button
+      {...props}
+      className='cursor-pointer rounded-full border border-solid border-transparent transition-colors ease-in-out flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto'
+    >
+      {children}
+    </button>
+  )
+);
+
+export default Button;
